082fb26214dfe94b6fbf92cb24d2c16fccb48d68
Sankalp-Yanamandra <174676664+Sankalp-Yanamandra@users.noreply.github.com>
2026-06-21 12:09:16 +0530

    state vector and calculation of prob using prob amplitudes using numpy

--- sandbox/week1_practice.py
+++ sandbox/week1_practice.py
@@ -1,1 +1,26 @@
-print('hello world')
+import numpy as np
+
+# define the amplitudes : eg. take alpha = 1/root2 = beta
+# alpha = 1 / np.sqrt(2)
+# beta = 1 / np.sqrt(2)
+
+alpha = 3/5
+beta = 4/5
+
+# state vector/qubit
+qubit = np.array([alpha, beta])
+
+# print(qubit)
+
+# modulus squares i.e. |alpha|^2
+prob_peace = pow(np.abs(qubit[0]),2)
+prob_conflict = pow(np.abs(qubit[1]),2)
+
+# printing the results
+
+print(f'Probability of peace : {prob_peace * 100:.2f}%')
+print(f'Probability of conflict : {prob_conflict * 100:.2f}%')
+
+# verifying the basic principle of prob : sum of all prob = 1
+prob_total = prob_peace + prob_conflict
+print(f'Total Probablity of occurence : {prob_total*100:.2f}%')
